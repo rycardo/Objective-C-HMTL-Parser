73b62ae27c73f75d1472058e79cdade8933b8b53
rycardo <rycardo@users.noreply.github.com>
2016-08-26 17:19:14 -0400

    Create HTMLParser+Extend.h

---
+++ HTMLParser+Extend.h
@@ -0,0 +1,26 @@
+//
+//  HTMLParser+Extend.h
+//  4XQ LLC
+//
+//  Created by 4XQ LLC on 26-08-16.
+//  Copyright (c) 2014 4XQ. All rights reserved.
+//
+//  Extended Ben Reeve's Objective-C-HTML-Parser
+
+#import "HTMLParser.h"
+
+@interface HTMLParser (Extend)
+
+- (HTMLNode *)findChildInNodes:(NSArray *)nodes 
+  withAttributesMatchingValues:(NSDictionary *)attributes 
+                  allowPartial:(BOOL)partial;
+
+- (NSArray *)findChildTagsOfNode:(HTMLNode *)node 
+                     withTagName:(NSString *)tagName 
+     andAttributesMatchingValues:(NSDictionary *)attributes 
+                    allowPartial:(BOOL)partial;
+
+- (NSArray *)findChildrenOfNode:(HTMLNode *)node 
+   WithAttributesMatchingValues:(NSDictionary *)attributes 
+                   allowPartial:(BOOL)partial;
+@end
