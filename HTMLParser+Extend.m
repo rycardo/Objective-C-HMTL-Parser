6fffed5cebedf72e5d1f697b3138c574762170b9
rycardo <rycardo@users.noreply.github.com>
2016-08-26 17:24:17 -0400

    Create HTMLParser+Extend.m

---
+++ HTMLParser+Extend.m
@@ -0,0 +1,11 @@
+//
+//  HTMLParser+Extend.m
+//  4XQ LLC
+//
+//  Created by 4XQ LLC on 26-08-16.
+//  Copyright (c) 2014 4XQ. All rights reserved.
+//
+//  Extended Ben Reeve's Objective-C-HTML-Parser
+
+#import "HTMLParser+Extend.h"
+
